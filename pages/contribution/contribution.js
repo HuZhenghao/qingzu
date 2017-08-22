fb147f41a47963073c68892f838aefc3b2522b68
LuckyJoker <1046944896@qq.com>
2017-08-22 16:22:06 +0800

    form

--- pages/contribution/contribution.js
+++ pages/contribution/contribution.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    image_src: [],
+    cycle_day: ["天", "月"],
+    index: 0,
+    date_start: ' 2017-09-01',
+    date_end: '2027-09-01'
   },
 
   /**
@@ -62,5 +66,37 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  addImage() {
+    const that = this;
+    wx.chooseImage({
+      sizeType: ['compressed'],
+      success: function (res) {
+        let tempFilePaths = res.tempFilePaths;
+        that.setData({ image_src: tempFilePaths });
+      },
+    })
+  },
+
+  bindPickerChange(e) {
+    this.setData({
+      index: e.detail.value
+    });
+  },
+
+  bindDateStartChange(e) {
+    this.setData({
+      date_start: e.detail.value
+    });
+  },
+  bindDateEndChange(e) {
+    this.setData({
+      date_end: e.detail.value
+    });
+  },
+
+  formSubmit(e){
+    console.log(e.detail);
   }
 })
--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    image_src: []
+    image_src: [],
+    cycle_day: ["天", "月"],
+    index:0,
+    date_start:' 2017-09-01',
+    date_end: '2027-09-01'
   },
 
   /**
@@ -68,10 +72,28 @@ Page({
   addImage() {
     const that = this;
     wx.chooseImage({
+      sizeType: ['compressed'],
       success: function(res) {
         let tempFilePaths = res.tempFilePaths;
         that.setData({image_src:tempFilePaths});
       },
     })
+  },
+
+  bindPickerChange(e) {
+    this.setData({
+      index: e.detail.value
+    });
+  },
+
+  bindDateStartChange(e) {
+    this.setData({
+      date_start: e.detail.value
+    });
+  },
+  bindDateEndChange(e) {
+    this.setData({
+      date_end: e.detail.value
+    });
   }
 })
